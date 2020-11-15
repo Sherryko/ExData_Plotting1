c0fc5bc371e813f22eaf5e38727d807c6aa439c9
Sherryko <lich3714@gmail.com>
2020-11-15 13:34:26 +0800

    plot1 v1

---
+++ plot1.R
@@ -0,0 +1,17 @@
+## Load data
+library(data.table)
+powerdata <- data.table::fread("household_power_consumption.txt", na.strings="?")
+
+## Convert the Date to Date class
+powerdata[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
+
+
+## Filter Dates 
+powerdata <- powerdata[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
+
+## Plot
+png("plot1.png", width = 480, height = 480 )
+hist(powerdata[, Global_active_power], main="Global Active Power",
+     xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "Red")
+
+dev.off()
